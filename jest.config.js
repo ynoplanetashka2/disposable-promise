5e2079a8b9f2ecd063f342fc5dd826476bf11010
Alexander Ermilov <mrynoplanetashka@gmail.com>
2023-10-05 19:18:35 +0500

    fix(jest.config.js): fixed wrong regex in jest config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {
-    '.*\\.spec\\.ts': ['ts-jest', {
+    '.*\\.spec\\.ts$': ['ts-jest', {
       tsconfig: 'tsconfig.jest.json',
     }]
   },
