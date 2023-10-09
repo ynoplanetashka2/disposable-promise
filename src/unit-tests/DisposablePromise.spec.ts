bb76e0671a5ebbd0ca876d1ae6233658ce429d09
Alexander Ermilov <mrynoplanetashka@gmail.com>
2023-10-09 05:07:46 +0500

    test(DisposablePromise): tests for new features added

--- src/unit-tests/DisposablePromise.spec.ts
+++ src/unit-tests/DisposablePromise.spec.ts
@@ -1,6 +1,40 @@
 import { DisposablePromise } from '../disposable-promise/DisposablePromise';
+import { AbortError } from '../disposable-promise/errors/AbortError';
 
 describe('DisposablePromise', () => {
+  it('should return instances of DisposablePromise on calls of then, catch, finally', async () => {
+    const methods = ['then', 'catch', 'finally'] as const;
+
+    for (const method of methods) {
+      const disposablePromise = new DisposablePromise((res) => void res(0));
+      expect((disposablePromise as any)[method](() => void 0)).toBeInstanceOf(
+        DisposablePromise,
+      );
+    }
+  });
+
+  it('should implement Symbol.toStringTag property', () => {
+    const disposablePromise = new DisposablePromise((res) => res(0));
+    expect(disposablePromise[Symbol.toStringTag]).toBe('DisposablePromise');
+    expect(Object.prototype.toString.call(disposablePromise)).toBe(
+      '[object DisposablePromise]',
+    );
+  });
+
+  it('should become settled asynchronously', async () => {
+    const cleanup = jest.fn();
+    const disposablePromise = new DisposablePromise((res) => {
+      res(1);
+      return cleanup;
+    });
+
+    // cleanup won't be called if disposablePromise settled, so we this hack to test if it settled
+    disposablePromise[Symbol.dispose]();
+    await disposablePromise;
+
+    expect(cleanup).toBeCalledTimes(1);
+  });
+
   describe('promise-like behaviour', () => {
     it('should run init-function synchronously(same as regular Promise)', () => {
       expect.assertions(1);
@@ -12,6 +46,13 @@ describe('DisposablePromise', () => {
     });
 
     describe('operations chaining', () => {
+      it('should be chainable with regular promises', async () => {
+        const resolved = DisposablePromise.resolve(Promise.resolve(1));
+        return resolved.then((value) => {
+          expect(value).toBe(1);
+        });
+      });
+
       const operations = {
         throwError: () => {
           throw new Error();
@@ -105,14 +146,13 @@ describe('DisposablePromise', () => {
 
   describe('cleanup', () => {
     describe('without chaining', () => {
-      it("should invoke passed cleanup function, when Symbol.dispose called, if promise isn't settled", () => {
+      it("should invoke passed cleanup function, when Symbol.dispose called, if promise isn't settled", async () => {
         const fn = jest.fn();
         const disposablePromise = new DisposablePromise<void>((res, _rej) => {
           // will be called on next tick, so promise will be hanging while cleanup is called
           setTimeout(res, 0);
           return fn;
         });
-
         disposablePromise[Symbol.dispose]();
 
         expect(fn.mock.calls.length).toBe(1);
@@ -155,20 +195,9 @@ describe('DisposablePromise', () => {
         });
       });
 
-      it('should return instances of DisposablePromise on calls of then, catch, finally', async () => {
-        const methods = ['then', 'catch', 'finally'] as const;
-
-        for (const method of methods) {
-          const disposablePromise = new DisposablePromise((res) => void res(0));
-          expect(
-            (disposablePromise as any)[method](() => void 0),
-          ).toBeInstanceOf(DisposablePromise);
-        }
-      });
-
       it('should be ok to pass undefined(dont return) cleanup function', async () => {
-        const withoutCleanup = new DisposablePromise(() => void 0);
-        expect(async () => withoutCleanup).not.toThrow();
+        const withoutCleanup = new DisposablePromise((res) => res(1));
+        await expect(withoutCleanup).resolves.toBe(1);
       });
 
       it('should throw if not undefined or function passed as cleanup', async () => {
@@ -177,30 +206,179 @@ describe('DisposablePromise', () => {
           const withForbiddenCleanup = new DisposablePromise(
             () => forbiddenCleanup as any,
           );
-          expect(withForbiddenCleanup).rejects.toThrow('unexpected value');
+          await expect(withForbiddenCleanup).rejects.toThrow(
+            'unexpected value',
+          );
         }
       });
 
-      it('should be chainable with regular promises', async () => {
-        const resolved = DisposablePromise.resolve(Promise.resolve(1));
-        return resolved.then((value) => {
-          expect(value).toBe(1);
+      it("should not raise an Error, if cleanup haven't raised an Error", async () => {
+        const disposablePromise = new DisposablePromise((res) => {
+          setTimeout(() => res(1), 0);
+          return () => void 0;
         });
+        disposablePromise[Symbol.dispose]();
+        await expect(disposablePromise).resolves.toBe(1);
+      });
+
+      it('should rethrow an error thrown by cleanup call', async () => {
+        const errorMessage = 'error text';
+        const disposablePromise = new DisposablePromise((res) => {
+          setTimeout(() => res(1), 0);
+          return () => {
+            throw new Error(errorMessage);
+          };
+        });
+        expect(() => {
+          disposablePromise[Symbol.dispose]();
+        }).toThrow(errorMessage);
+        await expect(disposablePromise).resolves.toBe(1);
       });
     });
 
-    describe('with chaining', () => {});
+    describe('with chaining', () => {
+      it("should raise AbortError on chanied promise's, if Symbol.dispose was called on chained promise while root promise was pending and root promise haven't rised an Error", async () => {
+        const disposablePromise = new DisposablePromise((res) => res(1));
+        const chained = disposablePromise.then(() => void 0);
 
-    describe('utility methods', () => {
-      it('should create resolved disposable promise with DisposablePromise.resolve', async () => {
-        const resolved = DisposablePromise.resolve(1);
-        expect(resolved).resolves.toBe(1);
+        chained[Symbol.dispose]();
+
+        await expect(disposablePromise).resolves.toBe(1);
+        await expect(chained).rejects.toThrow(AbortError);
       });
 
-      it('should create rejected disposable promise with DisposablePromise.reject', async () => {
-        const resolved = DisposablePromise.reject(new Error());
-        expect(resolved).rejects.toThrow();
+      it("should perform root promise cleanup when Symbol.dispose called on chained promise's", async () => {
+        expect.assertions(1);
+
+        const cleanup = jest.fn();
+        const disposablePromise = new DisposablePromise((res) => {
+          setTimeout(() => res(1), 0);
+          return cleanup;
+        });
+        const chained = disposablePromise.then(() => void 0);
+        chained.catch(() => void 0);
+
+        chained[Symbol.dispose]();
+
+        return Promise.resolve(disposablePromise).then(() => {
+          expect(cleanup).toBeCalledTimes(1);
+        });
       });
+
+      it('should perform root promise cleanup only once, when cleanup called multiple times on the same chained promise', async () => {
+        expect.assertions(1);
+
+        const cleanup = jest.fn();
+        const disposablePromise = new DisposablePromise((res) => {
+          setTimeout(() => res(1), 0);
+          return cleanup;
+        });
+        const chained = disposablePromise.then(() => void 0);
+        chained.catch(() => void 0);
+
+        chained[Symbol.dispose]();
+        chained[Symbol.dispose]();
+
+        return Promise.resolve(disposablePromise).then(() => {
+          expect(cleanup).toBeCalledTimes(1);
+        });
+      });
+
+      it('should perform root promise cleanup only once, when cleanup called from different chained promises', async () => {
+        expect.assertions(1);
+
+        const cleanup = jest.fn();
+        const disposablePromise = new DisposablePromise((res) => {
+          setTimeout(() => res(1), 0);
+          return cleanup;
+        });
+        const chained1 = disposablePromise.then(() => void 0);
+        const chained2 = disposablePromise.then(() => void 0);
+        chained1.catch(() => void 0);
+        chained2.catch(() => void 0);
+
+        chained1[Symbol.dispose]();
+        chained2[Symbol.dispose]();
+
+        return Promise.resolve(disposablePromise).then(() => {
+          expect(cleanup).toBeCalledTimes(1);
+        });
+      });
+
+      it('should propagate cleanup from chained promises to root with nested chaning', async () => {
+        expect.assertions(1);
+        const cleanup = jest.fn();
+        const disposablePromise = new DisposablePromise((res) => {
+          setTimeout(() => res(1), 0);
+          return cleanup;
+        });
+        const chained = disposablePromise.then(() => void 0);
+        const nestedChained = chained.then(() => void 0);
+        chained.catch(() => void 0);
+        nestedChained.catch(() => void 0);
+
+        nestedChained[Symbol.dispose]();
+
+        return Promise.resolve(disposablePromise).then(() => {
+          expect(cleanup).toBeCalledTimes(1);
+        });
+      });
+
+      it('should call cleanup of chained promise, if root promise already settled, chained promise is pending, and Symbol.dispose of chained promise was called', async () => {
+        expect.assertions(2);
+        const rootCleanup = jest.fn();
+        const chainedCleanup = jest.fn();
+        const disposablePromise = new DisposablePromise((res) => {
+          res(1);
+          return rootCleanup;
+        });
+        const chained = disposablePromise.then(() => {
+          return new DisposablePromise((res) => {
+            Promise.resolve()
+              .then(() => {
+                chained[Symbol.dispose]();
+              })
+              .then(() => {
+                res(2);
+              });
+            return chainedCleanup;
+          });
+        });
+
+        await chained;
+        expect(rootCleanup).toBeCalledTimes(0);
+        expect(chainedCleanup).toBeCalledTimes(1);
+      });
+
+      it("should not call cleanup on chained promise's if Symbol.dispose called on root promise and root promise already settled", async () => {
+        const cleaup = jest.fn();
+        const disposablePromise = new DisposablePromise((res) => {
+          res(1);
+          return cleaup;
+        });
+        const chained: DisposablePromise<unknown> = disposablePromise
+          .then(() => void chained[Symbol.dispose]())
+          .catch(() => void 0);
+
+        await chained;
+        expect(cleaup).toBeCalledTimes(0);
+      });
+
+      describe('stub', () => {});
+    });
+  });
+
+  describe('utility methods', () => {
+    it('should create resolved disposable promise with DisposablePromise.resolve', async () => {
+      const resolved = DisposablePromise.resolve(1);
+      expect(resolved).resolves.toBe(1);
+      expect(resolved).toBeInstanceOf(DisposablePromise);
+    });
+
+    it('should create rejected disposable promise with DisposablePromise.reject', async () => {
+      const rejected = DisposablePromise.reject(new Error());
+      expect(rejected).rejects.toThrow();
+      expect(rejected).toBeInstanceOf(DisposablePromise);
     });
   });
 });
